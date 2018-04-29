c196f1430699bbc40af52ec11f5fb71556e46e55
wjy <1096494284@qq.com>
2018-04-29 15:50:56 +0800

    添加记住密码功能

--- app/src/main/java/com/example/administrator/broadcastdemo/LoginActivity.java
+++ app/src/main/java/com/example/administrator/broadcastdemo/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.administrator.broadcastdemo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,13 +22,28 @@ public class LoginActivity extends BaseActivity {
     private EditText password_editText;
     private Button button;
 
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private CheckBox checkBox;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         account_editText = (EditText) findViewById(R.id.account);
         password_editText = (EditText) findViewById(R.id.password);
+        checkBox = (CheckBox) findViewById(R.id.checkbox);
         button = (Button) findViewById(R.id.button);
+        boolean isRemember = sharedPreferences.getBoolean("remember_password",false);
+        if (isRemember)
+        {
+            String account = sharedPreferences.getString("account","");
+            String password = sharedPreferences.getString("password","");
+            account_editText.setText(account);
+            password_editText.setText(password);
+            checkBox.setChecked(true);
+        }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +51,17 @@ public class LoginActivity extends BaseActivity {
                 String password = password_editText.getText().toString();
                 if(account.equals("admin") && password.equals("123456"))
                 {
+                    editor = sharedPreferences.edit();
+                    if(checkBox.isChecked())
+                    {
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else
+                    {
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent in = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(in);
                     finish();
